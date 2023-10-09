2962a6890cfada44c1524a3407a8e89f22588666
Fábio Borges <fabio.borges@nfon.com>
2023-10-09 17:15:45 +0100

    feat: typed propertydescriptor to protect method decorators

--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,10 +1,15 @@
 import "reflect-metadata";
 import { verb } from "./Types";
+import { RequestHandler } from "express";
+
+interface RouteHandlerDescriptor extends PropertyDescriptor {
+  value?: RequestHandler;
+}
 
 // factory decorators
 function routeBinder(method: verb) {
   return function get(path: string) {
-    return function (target: any, key: string, desc: PropertyDescriptor) {
+    return function (target: any, key: string, desc: RouteHandlerDescriptor) {
       Reflect.defineMetadata("path", path, target, key);
       Reflect.defineMetadata("method", method, target, key);
     };
